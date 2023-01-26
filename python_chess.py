b315ced2da3e44ed7c6224b3fc7005785b709738
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2023-01-26 19:26:19 +0100

    Implemented stockfish as an engine
    Bugg on second iteration of play

--- python_chess.py
+++ python_chess.py
@@ -1,31 +1,80 @@
 import chess
+import chess.engine
+import subprocess
+from stockfish import Stockfish
 
-#Creates a new board
+# Creates a new board
 board = chess.Board()
 
-#Validate the move
+# Start the engine
+engine = Stockfish(path="/opt/homebrew/Cellar/stockfish/15.1/bin/stockfish")
+
+# Initialize the moves list
+moves = []
+list_moves_uci = []
+
+
+# Validate the move
 while not board.is_game_over():
-    #Get the move from the User
-    move = input("Enter a move in SAN notation: ")
-    #Validate the move
+
+    # Get the move from the User
+
+    print("Enter a move in SAN notation")
+    user_move = input()
+
+    # Validate the move
     try:
-        move = board.parse_san(move)
-        board.push(move)
+
+        # Making a user move
+        board_move =  board.parse_san(user_move)
+        board.push(board_move)
+
+        #Formating the moves in uci format
+        moves_uci = board_move.uci()
+        list_moves_uci.append(moves_uci)
+        print(list_moves_uci)
+
+        # Validade the move and print board
         print("Move has been executed and is valid")
-        #Checks if the move is either check or checkmate
-        print("Current board:")
         print(board)
+
+        # Check the list
+        print(list_moves_uci)
+
+        # Making the engine move
+        engine.make_moves_from_current_position(list_moves_uci)
+        engine_move = engine.get_best_move(1000)
+        board_move_engine =  board.parse_san(engine_move)
+        board.push(board_move_engine)
+        list_moves_uci.append(engine_move)
+        print(list_moves_uci)
+
+
+
+        #print the engine move
+        print(board)
+
+        # Check for checks ;)
         if board.is_check():
             print("Check!")
         if board.is_checkmate():
             print("You got checkmated you lost!")
             break
+
+    # Throw exception
     except ValueError:
         print("Invalid move")
 
-#Save on PNG noation
+# Save the game in PGN notation
 with open("game.pgn", "w") as f:
-    f.write(board.pgn)
+    f.write(board.pgn())
 
 
-#TODO: IMPLEMENT LELAA CHESS ENGINE
+
+#TODO: IMPLEMENT STOCKFISH!!
+
+#DAY 1: IMPLEMENT THE IDEA... USING python chess library
+#DAY 2: Impleneted check and checkmate feature and tried to implement a chess engine did not work
+#DAY 3: #TODO: IMPLEMENT LELAA CHESS ENGINE OR STOCKFISH!!
+#DAY 3: Debugging
+#DAY 4: IMPLEMENTED STOCKFISH bugg on second interaction
